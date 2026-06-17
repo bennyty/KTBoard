967e17d724e0a191dfecc028881af2a61086122c
Ben Espey <benjamin.espey@gmail.com>
2026-06-17 15:21:46 -0400

    feat(maps): add volkus 1-6

--- src/data/registry.ts
+++ src/data/registry.ts
@@ -1,6 +1,11 @@
 import type { AnnotatedMap, KillzoneCatalogue } from '@/model/types'
 import volkusCatalogueJson from './volkus-catalogue.json'
 import volkus1Json from './volkus-1.json'
+import volkus2Json from './volkus-2.json'
+import volkus3Json from './volkus-3.json'
+import volkus4Json from './volkus-4.json'
+import volkus5Json from './volkus-5.json'
+import volkus6Json from './volkus-6.json'
 import one_obj_map from './one-obj-test-map.json'
 
 export const catalogues: Record<string, KillzoneCatalogue> = {
@@ -9,6 +14,11 @@ export const catalogues: Record<string, KillzoneCatalogue> = {
 
 export const maps: AnnotatedMap[] = [
   volkus1Json as AnnotatedMap,
+  volkus2Json as AnnotatedMap,
+  volkus3Json as AnnotatedMap,
+  volkus4Json as AnnotatedMap,
+  volkus5Json as AnnotatedMap,
+  volkus6Json as AnnotatedMap,
   one_obj_map as AnnotatedMap
 ]
 
